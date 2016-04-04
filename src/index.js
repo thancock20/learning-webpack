23ddb061c3bba9a3a713688f101f919d7afd55a5
Todd Hancock <thancock20@hotmail.com>
2016-04-04 18:36:30 -0500

    Setup production environment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// If we have an anchor, render the Button component on it
 if (document.querySelectorAll('a').length) {
   require.ensure([], () => {
     const Button = require('./Components/Button').default;
     const button = new Button('google.com');
 
     button.render('a');
-  }, 'button');
+  });
+}
+
+// If we have a title, render the Header component on it
+if (document.querySelectorAll('h1').length) {
+  require.ensure([], () => {
+    const Header = require('./Components/Header');
+
+    new Header().render('h1');
+  });
 }
